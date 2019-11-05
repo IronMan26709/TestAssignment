adb49d17e793702c52801b78958b7e2fd527e9f0
IronMan <ironmanandrej@gmail.com>
2019-11-05 18:44:21 +0200

    addet build

---
+++ src/build/precache-manifest.a681a0b43ab22fa6d33cbcc9b325e925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda41c3675c41206b84681b7584a0738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d259d1c4e7b2900df1d7",
+    "url": "/static/css/main.626e11d8.chunk.css"
+  },
+  {
+    "revision": "4f4cf44056e2b341c795",
+    "url": "/static/js/2.7a2b3aa5.chunk.js"
+  },
+  {
+    "revision": "d259d1c4e7b2900df1d7",
+    "url": "/static/js/main.6e340cb0.chunk.js"
+  },
+  {
+    "revision": "72cf547c9d12782e15f0",
+    "url": "/static/js/runtime-main.78d77056.js"
+  }
+]);
